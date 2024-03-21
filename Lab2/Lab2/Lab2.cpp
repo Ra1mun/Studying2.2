0d0ae1e9ef348a839a09f42da2818b7c096746f3
Lost_Shadow <sobolm58@gmail.com>
2024-03-21 19:00:58 +0700

    add outputfile

--- Lab2/Lab2/Lab2.cpp
+++ Lab2/Lab2/Lab2.cpp
@@ -1,5 +1,6 @@
 ﻿#include "Sort.h"
 #include "User.h"
+#include <fstream>
 
 int main()
 {
@@ -11,34 +12,40 @@ int main()
     int* id = new int[n];
     User** users = new User*[n];
 
+    ofstream outputFile("result.txt");
+
     for (int i = 0; i < n; i++) {
         users[i] = new User();
         id[i] = i;
     }
 
     Sort<User*> s;
-    cout << "Сортировка Шелла" << endl;
+
+    outputFile << "Сортировка Шелла" << endl;
     s.ShellSort(users, id, n);
-    if (s.IsSorted(users, id, n)) {
+    if (s.IsSorted(users, id, n) && outputFile.is_open()) {
         for (int i = 0; i < n; i++) {
-            cout << i + 1 << "место: " << *users[id[n - i - 1]];
-            cout << endl;
+            outputFile << i + 1 << "место: " << *users[id[n - i - 1]] << "  " << id[n - i - 1];
+            outputFile << endl;
         }
     }
 
-    cout << endl;
+    outputFile << endl;
 
     for (int i = 0; i < n; i++) {
         id[i] = i;
     }
 
-    cout << "Пирамидальная сортировка" << endl;
+    outputFile << "Пирамидальная сортировка" << endl;
     s.PiramidSort(users, id, n);
-    for (int i = 0; i < n; i++) {
-        cout << i + 1 << "место: " << *users[id[n - i - 1]];
-        cout << endl;
+    if (s.IsSorted(users, id, n) && outputFile.is_open()) {
+        for (int i = 0; i < n; i++) {
+            outputFile << i + 1 << "место: " << *users[id[n - i - 1]] << "  " << id[n - i - 1];
+            outputFile << endl;
+        }
     }
 
+    outputFile.close();
 
     for (int i = 0; i < n; i++) {
         delete[] users[i];
