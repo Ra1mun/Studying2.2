842be988ac0001ea76b8e8b1674d9a97198bd86d
Lost_Shadow <69916626+Ra1mun@users.noreply.github.com>
2024-06-04 02:11:33 +0700

    add lab 7 and 8

---
+++ Параллельное программирование/lab7/lab7/lab7.cpp
@@ -0,0 +1,84 @@
+﻿#include <iostream>
+#include <stdlib.h>
+#include <cmath>
+#include <omp.h>
+#include <random>
+
+#define N 1000
+#define SIZE 2
+
+void fillMatrix(double** A, int size) {
+    using namespace std;
+
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_real_distribution<double> dis(0.0, 1.0);
+
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            A[i][j] = dis(gen);
+        }
+    }
+}
+
+int main()
+{
+    omp_set_num_threads(SIZE);
+
+    double** A = new double*[N];
+    for (int i = 0; i < N; i++) {
+        A[i] = new double[N];
+    }
+
+    double** B = new double* [N];
+    for (int i = 0; i < N; i++) {
+        B[i] = new double[N];
+    }
+
+    double** C = new double* [N];
+    for (int i = 0; i < N; i++) {
+        C[i] = new double[N];
+        for (int j = 0; j < N; j++) {
+            C[i][j] = 0;
+        }
+    }
+
+    // Инициализация матриц A и B
+    fillMatrix(A, N);
+    fillMatrix(B, N);
+
+    // Вариант циклов i-j-k
+    double start_time = omp_get_wtime();
+#pragma omp parallel for shared(A, B, C) collapse(3) schedule(static)
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            for (int k = 0; k < N; k++) {
+                C[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
+    double end_time = omp_get_wtime();
+    std::cout << "Time for i-j-k loop: " << end_time - start_time << " seconds" << std::endl;
+
+    // Обнуление матрицы C для следующего варианта циклов
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            C[i][j] = 0.0;
+        }
+    }
+
+    // Вариант циклов k-i-j
+    start_time = omp_get_wtime();
+#pragma omp parallel for shared(A, B, C) collapse(3) schedule(static)
+    for (int k = 0; k < N; k++) {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                C[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
+    end_time = omp_get_wtime();
+    std::cout << "Time for k-i-j loop: " << end_time - start_time << " seconds" << std::endl;
+
+    return 0;
+}
---
+++ Параллельное программирование/lab8/lab8/lab8.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <omp.h>
+
+#define SIZE 12
+
+int const n = 10e8;
+
+double f(double x) {
+    return exp(-1.46 * x * x) / (3.5 + sin(x));
+}
+
+void CheckAnswer(double actual) {
+    using namespace std;
+
+    double exception = 0.08;
+
+    cout << "Ответ: " << actual << '\n';
+    cout << "Ожидаемый ответ: " << exception << '\n';
+    cout << "Погрешность: " << abs(actual - exception) << '\n';
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    omp_set_num_threads(SIZE);
+
+    double a = 0.3;
+    double b = 0.8;
+
+    double integral = 0.0;
+    double h = (b - a) / n;
+
+    clock_t start_time = clock();
+
+#pragma omp parallel
+    {
+#pragma omp for reduction(+ : integral)
+        for (int step = 0; step < n / SIZE; step++) {
+            double x1 = a + step * h;
+            double x2 = a + (step + 1) * h;
+            integral += (x2 - x1) / 6.0 * (f(x1) + 4.0 * f(0.5 * (x1 + x2)) + f(x2));
+        }
+    }
+
+    CheckAnswer(integral);
+    std::cout << "Время: " << (double)(clock() - start_time) << '\n';
+}
